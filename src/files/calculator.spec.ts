adf7a549646dbc628db7b2eaca42b7698d1da8bb
emelymack <emelymack@gmail.com>
2023-06-13 20:48:10 -0300

    Implemento tests

--- src/files/calculator.spec.ts
+++ src/files/calculator.spec.ts
@@ -2,17 +2,66 @@ import calculator from "../files/calculator";
 
 describe("calculadora", () => {
     it("Add 1 to 2 should return 3", () => {
+        // Arrange
+        const number1 = 1
+        const number2 = 2
+        const expected = 3
+
+        // Act
+        const result = calculator.add(number1, number2);
+
+        // Assert
+        expect(result).toBe(expected)
     });
 
     it("Subtract 2 from 10 should return 8", () => {
+        // Arrange
+        const number1 = 10
+        const number2 = 2
+        const expected = 8
+
+        // Act
+        const result = calculator.subtract(number1, number2);
+
+        // Assert
+        expect(result).toBe(expected)
     });
 
     it("Multiply  2 with 8 should return 16", () => {
+        // Arrange
+        const number1 = 2
+        const number2 = 8
+        const expected = 16
+
+        // Act
+        const result = calculator.multiply(number1, number2);
+
+        // Assert
+        expect(result).toBe(expected)
     });
 
     it("Multiply should be idempotent", () => {
+        // Arrange
+        const number = 1
+        const expected = 1
+
+        // Act
+        const result = calculator.multiply(number, number);
+
+        // Assert
+        expect(result).toBe(expected)
     });
 
     it("Divide 8 with 2 should return 4", () => {
+        // Arrange
+        const number1 = 8
+        const number2 = 2
+        const expected = 4
+
+        // Act
+        const result = calculator.divide(number1, number2);
+
+        // Assert
+        expect(result).toBe(expected)
     });
 });
--- src/files/dummy.spec.ts
+++ src/files/dummy.spec.ts
@@ -1,12 +1,31 @@
 import dummy from "../files/dummy";
 
 describe("dummy", () => {
+    const mockCallback = jest.fn()
+
     describe("when shouldTrigger is false", () => {
         it("should not call callback", () => {
+            // Arrange
+            const shouldTrigger = false
+
+            // Act
+            dummy(shouldTrigger, mockCallback)
+
+            // Assert
+            expect(mockCallback).not.toBeCalled()
+            
         });
     });
     describe("when shouldTrigger is true", () => {
         it("should call callback", () => {
+            // Arrange
+            const shouldTrigger = true
+
+            // Act
+            dummy(shouldTrigger, mockCallback)
+
+            // Assert
+            expect(mockCallback).toBeCalled()
         });
     });
 });
